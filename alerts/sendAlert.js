9482749105d967fa005b15cd59be34cf385e3413
kovacs7 <asishkumarbehera.official@gmail.com>
2025-05-18 14:07:01 +0530

    Discord dummy URL moved to .env file.

--- alerts/sendAlert.js
+++ alerts/sendAlert.js
@@ -1,8 +1,8 @@
 const axios = require("axios");
-const WEBHOOK_URL = "https://discord.com/api/webhooks/your-webhook-url"; // Replace
+require("dotenv").config();
 
 axios
-  .post(WEBHOOK_URL, {
+  .post(process.env.DISCORD_WEBHOOK_URL, {
     content: "🚨 AutoHeal Alert: High resource usage or service down!",
   })
   .then(() => console.log("Alert sent"))
